2340e945d645ca6f8f3864df99be5febe6cf22f4
Rick <rjones@digitalseat.com>
2023-05-29 12:13:14 -0500

    Added all CRUD routes for /reviewers

--- amplify/backend/function/reviewersFn/src/controllers/reviewerController.js
+++ amplify/backend/function/reviewersFn/src/controllers/reviewerController.js
@@ -2,3 +2,7 @@ const factory = require('./handlerFactory.js')
 const Reviewer = require('../models/reviewerModel.js')
 
 exports.getAllReviewers = factory.getAll(Reviewer)
+exports.getOne = factory.getOne(Reviewer)
+exports.createReviewer = factory.createOne(Reviewer)
+exports.updateOne = factory.updateOne(Reviewer)
+exports.deleteOne = factory.deleteOne(Reviewer)
