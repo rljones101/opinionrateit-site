ddac7ec4e5c59a796bdecdf9bac9c90d0b282a96
Rick <rjones@digitalseat.com>
2023-05-28 21:39:17 -0500

    Changed the nav links back to use the router for navigation

--- src/components/buttons/buttonNav.vue
+++ src/components/buttons/buttonNav.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 
-defineProps<{
+const router = useRouter()
+
+const props = defineProps<{
   link: string,
   path: string
 }>()
-
+const goToPath = () => {
+  router.push(props.path)
+}
 </script>
 
 <template>
   <div class="relative group ml-4 mr-4">
-    <a :href="path" class="flex transition ease-out hover:ease-in duration-300 pt-2 pb-2 text-white font-bold">{{link}}</a>
+    <button class="flex transition ease-out hover:ease-in duration-300 pt-2 pb-2 text-white font-bold" @click="goToPath">{{link}}</button>
     <span class="absolute -bottom-1 w-0 h-1 bg-orange-500 transition-all group-hover:w-full"></span>
   </div>
 </template>
